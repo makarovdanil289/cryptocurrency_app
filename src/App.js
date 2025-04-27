93ef2eeb0ae7872fb7511cf08ba9ee4360e5ab52
Danil <makarovdanil289@gmail.com>
2025-04-27 14:00:23 +0500

    Added navbar, homepage, layout and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, BrowserRouter as Router, Routes, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import {Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News} from './components';
+
+import './index.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className="App">
+        <div className='navbar'>
+          <Navbar />
+        </div>
+        <div className='main'>
+          <Layout>
+            <div className='routes'>
+              <Routes>
+                <Route path='/' element={<Homepage />} />
+                <Route path='/exchanges' element={<Exchanges />} />
+                <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+                <Route path='/crypto/:coinId' element={<CryptoDetails />} />
+                <Route path='/news' element={<News />} />
+              </Routes>
+            </div>
+          </Layout>
+        </div>
+        <div className='footer'>
+          <Typography.Title  level={5} style={{color: 'white', textAlign: 'center'}}>
+            Cryptoverse
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </Router>
   );
 }
 
